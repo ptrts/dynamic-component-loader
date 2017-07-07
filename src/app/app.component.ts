a3352cb4f2ae7b325e5fd15767de8fed926f26a6
Pavel Taruts <taruts@improveitgroup.com>
2017-07-07 13:18:01 +0700

    Первый коммит

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {AdItem} from './ad-item';
+
+import {AdService} from './ad.service';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div>
+            <add-banner [ads]="ads"></add-banner>
+        </div>
+    `
+})
+export class AppComponent implements OnInit {
+
+    // AdItem - это пара:
+    //      Тип компонента
+    //      Данные компонента
+    // Соответственно, и компоненты должны быть не абы какие, а поддерживающие некий интерфейс
+    // Интерфейс этот - это AdComponent
+    // Интерфейс это такой, что надо иметь публичное свойство data, которое можно установить, и которое можно считать
+    // data - она произвольного формата, тут у компонентов есть свобода и гибкость
+    // 
+    // Таким образом, AdItem - это такая инструкция для создания компонентов, поддерживающих интерфейс AdComponent. 
+    //
+    // Вот. И здесь у нас как раз есть список таких инструкций для создания компонентов. 
+    ads: AdItem[];
+
+    constructor(private adService: AdService) {
+    }
+
+    ngOnInit() {
+
+        // Этот список мы получаем из специального сервиса. 
+        // Хотя, мы могли бы делать этот список прямо тут у себя. 
+        this.ads = this.adService.getAds();
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AdBannerComponent} from './ad-banner.component';
+import {AdDirective} from './ad.directive';
+import {AdService} from './ad.service';
+import {AppComponent} from './app.component';
+import {HeroJobAdComponent} from './hero-job-ad.component';
+import {HeroProfileComponent} from './hero-profile.component';
+
+@NgModule({
+
+    imports: [
+        BrowserModule
+    ],
+
+    providers: [
+        AdService
+    ],
+
+    declarations: [
+        AppComponent,
+        AdBannerComponent,
+        HeroJobAdComponent,
+        HeroProfileComponent,
+        AdDirective
+    ],
+
+    entryComponents: [
+        HeroJobAdComponent,
+        HeroProfileComponent
+    ],
+
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {
+    constructor() {
+    }
+}
